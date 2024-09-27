96e46b06848e6cb4f8b780fb018444592684bed1
Its-Ackarax <DeanRigneyDev@gmail.com>
2024-09-27 19:08:16 +0100

    Information gatherer created.
    Information gatherer created. Yet to link already set up features with it.

--- src/DailyCalorieCalc.java
+++ src/DailyCalorieCalc.java
@@ -1,10 +1,75 @@
 
 public class DailyCalorieCalc 
 {
-	double userWeight= 0.0;
+	double userWeight = 0.0;
+	int userHeight = 0;
+	String userGender = "";
+	int userAge = 0;
 	
-	private void MaintenanceCalc()
+	private int BMRCalc()
 	{
-		int maintenenceResult = userWeight * 15;
+		
+		int basalMetabolicRate;
+		
+		if (userGender == "M")
+		{
+			basalMetabolicRate = (int) Math.round(10 * userWeight + 6.25 * userHeight - 5 * userAge + 5);
+		}
+		else if (userGender == "F");
+		{
+			basalMetabolicRate = (int) Math.round(10 * userWeight + 6.25 * userHeight - 5 * userAge - 161);
+		}
+		return basalMetabolicRate;
+	}
+	
+	private double getActivityLevelNumber(int userActivityLevel)
+	{
+		double activityLevelNumber = 0;
+		
+		switch(userActivityLevel)
+		{
+			case 1:
+				//Not Active
+				activityLevelNumber = 1.55;
+			break;
+			
+			case 2:
+				//Slightly Active
+				activityLevelNumber = 1.70;
+				
+			break;
+			
+			case 3:
+				//Moderately Active
+				activityLevelNumber = 1.85;
+				
+			break;
+			
+			case 4:
+				//Very Active
+				activityLevelNumber = 2.025;
+				
+			break;
+			
+			case 5:
+				// Vigorously Active
+				activityLevelNumber = 2.2;
+				
+			break;
+			
+			case 6:
+				//Extremely Active
+				activityLevelNumber = 2.4;
+				
+			break;
+		}
+ 
+		return activityLevelNumber;
+	}
+	private double MaintenanceCalc(int bmr, double userActivityLevel)
+	{
+		
+		double maintenenceResult = bmr * userActivityLevel;
+		return maintenenceResult;
 	}
 }
---
+++ src/InformationGather.java
@@ -0,0 +1,50 @@
+import java.io.DataInputStream;
+
+public class InformationGather 
+{
+
+	DataInputStream in = new DataInputStream(System.in);
+
+	int userAge = 0;
+	String userGender = "";
+	int userHeightInCM = 0;
+	double userWeightInKG = 0.0;
+	String userActivityLevel = "";
+	
+	private void InfoGather()
+	{
+		
+		try
+		{
+			System.out.println("Before we begin I need to gather some information from you.");
+			System.out.println("Please enter your age:");
+			userAge = Integer.parseInt(in.readLine());
+			
+			System.out.println("Please enter your gender. 'M' for Male 'F' for Female:");
+			userGender = in.readLine();
+			
+			System.out.println("Please enter your height in 'Centimeters:");
+			userHeightInCM = Integer.parseInt(in.readLine());
+			
+			System.out.println("Please enter your weight in 'KG's:");
+			userWeightInKG = Double.parseDouble(in.readLine());
+			
+			System.out.println("Please enter the number that best corresponds to your activity level:");
+			System.out.println("Enter '1' if 'Not Active'.");
+			System.out.println("Enter '2' if 'Slightly Active'.");
+			System.out.println("Enter '3' if 'Moderately Active'.");
+			System.out.println("Enter '4' if 'Very Active'.");
+			System.out.println("Enter '5' if 'Vigorously Active'.");
+			System.out.println("Enter '5' if 'Extreamely Active'.");
+			System.out.println("Enter your number now:");
+
+			userActivityLevel = in.readLine();	
+
+		}
+		catch(Exception e)
+		{
+			
+		}
+	
+	}
+}
